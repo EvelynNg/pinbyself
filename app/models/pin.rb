41d405ce143cce8057660a99b5c464f485977e1e
EvelynNg <Evelynng312@gmail.com>
2014-11-04 11:26:38 -0800

    update

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,10 @@ class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :medium => "300x300>"}
      validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+    validates :image, presence: true
+  	validates :description, presence: true
+
+
 end
 
